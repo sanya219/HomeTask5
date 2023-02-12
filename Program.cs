89bf78b731406050b170d565f6d2bf327fc9648d
Alexander Daurskikh <sanya219@gmail.com>
2023-02-12 07:51:26 +0100

    Task 36 solved

--- Program.cs
+++ Program.cs
@@ -33,3 +33,28 @@ int[] myArray34 = CreateArray(arraySize34, 100, 999);
 PrintArray(myArray34);
 int num34 = NumberOfEvenElements(myArray34);
 Console.WriteLine("Number of even elements in array: " + num34);
+
+/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+int SumOfElementsOnOddPositions(int[] array)
+{
+    int result = 0;
+    if(array.Length > 1)
+        for(int i = 1; i < array.Length; i += 2)
+            result += array[i];
+    return result;
+}
+
+Console.Write("Enter the array size: ");
+int arraySize36 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the minimum possible value: ");
+int arrayMin36 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the maximum possible value: ");
+int arrayMax36 = Convert.ToInt32(Console.ReadLine());
+int[] myArray36 = CreateArray(arraySize36, arrayMin36, arrayMax36);
+PrintArray(myArray36);
+int sum36 = SumOfElementsOnOddPositions(myArray36);
+Console.WriteLine("Number of elements on odd positions in array: " + sum36);
